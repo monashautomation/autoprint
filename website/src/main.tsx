e63112dcde058707efe41fe12e357e2e67c6e36a
Jicheng Zhi <jzhi0004@student.monash.edu>
2024-11-24 21:09:10 +1100

    chore: add react router

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+
 import "./index.css";
-import App from "./App.tsx";
+
+import { DashboardPage } from "@/pages/dashboard";
 
 const root = document.getElementById("root");
 
 if (root) {
 	createRoot(root).render(
 		<StrictMode>
-			<App />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" index element={<DashboardPage />} />
+				</Routes>
+			</BrowserRouter>
 		</StrictMode>,
 	);
 }
